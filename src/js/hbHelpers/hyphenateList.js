9794020d21c239857f06481afe4e23624437dc65
Samuel Chittenden <sbchittenden@gmail.com>
2017-12-04 19:57:16 -0700

    cleared out console.logs from helpers

--- src/js/hbHelpers/hyphenateList.js
+++ src/js/hbHelpers/hyphenateList.js
@@ -1,7 +1,6 @@
 const Handlebars = require('handlebars');
 
 module.exports = function hyphenateList(techArray) {
-  // console.log('what up from the hyphenateList helper! ', techArray);
   var result = techArray.map(item => item.toUpperCase()).join(' - ');
   return new Handlebars.SafeString(result);
 };
--- src/js/hbHelpers/removeHash.js
+++ src/js/hbHelpers/removeHash.js
@@ -1,7 +1,6 @@
 const Handlebars = require('handlebars');
 
 module.exports = function removeHash() {
-  // console.log('what up from the removeHash helper! ');
   var str = this.projectLink.substr(1);
   return new Handlebars.SafeString(str);
 };
